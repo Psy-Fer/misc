6e479260369e7b655f4de98e4c12126bfa49e787
Psy-Fer <fergo1988@gmail.com>
2021-05-11 15:56:20 +1000

    Copyright fix

--- scripts/reorgcirc.py
+++ scripts/reorgcirc.py
@@ -37,7 +37,7 @@ import edlib
     ----------------------------------------------------------------------------
     MIT License
 
-    Copyright (c) 2019 James Ferguson
+    Copyright (c) 2021 James Ferguson
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
